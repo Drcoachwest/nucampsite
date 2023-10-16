7b0dc7638900afafa6311512827c0deb08608f5c
Michael West <michael.west@gpisd.org>
2023-10-16 11:33:51 -0500

    working campsite onclick

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -4,13 +4,17 @@ import CampsiteCard from './CampsiteCard';
 import { selectAllCampsites } from './campsitesSlice'
 
 
-const CampsitesList = () => {
+const CampsitesList = ({ setCampsiteId }) => {
   const campsites = selectAllCampsites();
   return (
     <Row className = 'ms-auto'>
       {campsites.map((campsite) => {
         return (
-          <Col md='5' classname = 'm-4' key = {campsite.id}>
+          <Col 
+          md='5' 
+          classname = 'm-4' 
+          key = {campsite.id}
+          onClick= {() => setCampsiteId(campsite.id)}>
             <CampsiteCard campsite={campsite} />
           </Col>
         );
