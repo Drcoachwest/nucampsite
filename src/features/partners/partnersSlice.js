f1a2896e929dbe5dcd7dd948dfc8078d557fa21c
Michael West <michael.west@gpisd.org>
2023-10-19 15:29:51 -0500

    end of week2 including workshop assignment

---
+++ src/features/partners/partnersSlice.js
@@ -0,0 +1,13 @@
+import { PARTNERS } from '../../app/shared/PARTNERS';
+
+export const selectAllPartner = () => {
+  return PARTNERS;
+};
+
+export const selectFeaturedPartner = () => {
+  return PARTNERS.find((partner) => partner.featured);
+};
+
+
+
+
