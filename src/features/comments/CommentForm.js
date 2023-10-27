e0de4efbff95120c83cb834019f86e3e2ccb27fa
Michael West <michael.west@gpisd.org>
2023-10-27 12:32:23 -0500

    end of week 3 fixed comment field

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,91 +1,93 @@
-import { useState } from'react';
-import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
-import { Formik, Field, Form, ErrorMessage} from'formik';
-import validateCommentForm from '../../utils/validateCommentForm';
-const CommentForm = ({campsiteId}) => {
+import { useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  Label,
+} from "reactstrap";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import validateCommentForm from "../../utils/validateCommentForm";
+
+
+const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setmodalOpen] = useState(false);
   const handleSubmit = (values) => {
     const comment = {
       campsiteId: parseInt(campsiteId),
-            rating: values.rating,
-            author: values.author,
-            text: values.commentText
+      rating: values.rating,
+      author: values.author,
+      text: values.commentText,
     };
-    console.log(comment)
-    setmodalOpen(false)
-  }
+    console.log(comment);
+    setmodalOpen(false);
+  };
 
   return (
     <>
-    <Button outline onClick={() => setmodalOpen(true)}>
-        <i className='fa fa-pencil fa-lg' /> Add Comment
-    </Button>
-    <Modal isOpen={modalOpen}>
-        <ModalHeader toggle = {()=> setmodalOpen(false)}>
-            Add Comment
+      <Button outline onClick={() => setmodalOpen(true)}>
+        <i className="fa fa-pencil fa-lg" /> Add Comment
+      </Button>
+      <Modal isOpen={modalOpen}>
+        <ModalHeader toggle={() => setmodalOpen(false)}>
+          Add Comment
         </ModalHeader>
-        campsite: {campsiteId}
-        <Formik initialValues = {{
-          rating : undefined,
-          author : " ",
-          commentText : " "
+        <ModalBody>
+        
+        <Formik
+          initialValues={{
+            rating: undefined,
+            author: " ",
+            commentText: " ",
           }}
-          onSubmit = {handleSubmit}
-          validate={validateCommentForm}>
-              <Form>
-                  <FormGroup>
-                      <Label htmlFor='rating'>
-                        Rating
-                      </Label>
-                      <Field
-                                    name='rating'
-                                    as='select'
-                                    className='form-control'
-                                >
-                                    <option>Select...</option>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                </Field>
-                      <ErrorMessage name='rating'>
-                      {(msg) => <p className='text-danger'>{msg}</p>}
-                      </ErrorMessage>
-                  </FormGroup>
-                  <FormGroup>
-                      <Label htmlFor='author'>
-                        Your Name
-                      </Label>
-                      <Field
-                                    name='author'
-                                    placeholder='Your Name'
-                                    className='form-control'
-                                />
-                      <ErrorMessage name='author'>
-                         {(msg) => <p className='text-danger'>{msg}</p>}
-                      </ErrorMessage>
-                  </FormGroup>
-                  <FormGroup>
-                      <Label htmlFor='commentText'>
-                        Comment
-                      </Label>
-                      <Field
-                                    name='commentText'
-                                    as='textarea'
-                                    rows='12'
-                                    className='form-control'
-                                />
-                  </FormGroup>
-                  <Button type='submit' color='primary'>
-                                Submit
-                            </Button>
-              </Form>
-
+          onSubmit={handleSubmit}
+          validate={validateCommentForm}
+        >
+          <Form>
+            <FormGroup>
+              <Label htmlFor="rating">Rating</Label>
+              <Field name="rating" as="select" className="form-control">
+                <option>Select...</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Field>
+              <ErrorMessage name="rating">
+                {(msg) => <p className="text-danger">{msg}</p>}
+              </ErrorMessage>
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="author">Your Name</Label>
+              <Field
+                name="author"
+                placeholder="Your Name"
+                className="form-control"
+              />
+              <ErrorMessage name="author">
+                {(msg) => <p className="text-danger">{msg}</p>}
+              </ErrorMessage>
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="commentText">Comment</Label>
+              <Field
+                name="commentText"
+                as="textarea"
+                rows="12"
+                className="form-control"
+              />
+            </FormGroup>
+            <Button type="submit" color="primary">
+              Submit
+            </Button>
+          </Form>
         </Formik>
-    </Modal>
+        </ModalBody>
+      </Modal>
     </>
-  )
+  );
 };
 
 export default CommentForm;
--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -3,27 +3,25 @@ import Comment from "./Comment";
 import { selectCommentsByCampsiteId } from "./commentsSlice";
 import CommentForm from "./CommentForm";
 
-const CommentsList = ({campsiteId }) => {
+const CommentsList = ({ campsiteId }) => {
   const comments = selectCommentsByCampsiteId(campsiteId);
 
-    if (comments && comments.length > 0) {
-      return (
-        <Col md='5' className= 'm-1'>
-            <h4>Comments</h4>
-            {comments.map((comment) => {
-                return <Comment key={comment.id} comment={comment} />
-            })}
-            <CommentForm campsiteId={campsiteId}>
-
-            </CommentForm>
-        </Col>
-      );
-    }
+  if (comments && comments.length > 0) {
     return (
-      <Col md='5' className="m-1">
-          There is no comments for this campsite yet.
+      <Col md="5" className="m-1">
+        <h4>Comments</h4>
+        {comments.map((comment) => {
+          return <Comment key={comment.id} comment={comment} />;
+        })}
+        <CommentForm campsiteId={campsiteId}></CommentForm>
       </Col>
-    )
+    );
+  }
+  return (
+    <Col md="5" className="m-1">
+      There is no comments for this campsite yet.
+    </Col>
+  );
 };
 
 export default CommentsList;
